d0c773e10cffd6d6be6ad0c4e2a9eb93a3440079
MityaDementiy <d.dementiy@gmail.com>
2020-10-16 16:27:38 +0300

    Make channels components, change channels styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Channels from './Channels';
 import ChatRoom from './ChatRoom';
 import MessageForm from './MessageForm';
 
 const App = ({ channels }) => {
-  const renderChannels = (channels) => {
-    return channels.map((channel) => <li key={channel.name}><a href='#'>{channel.name}</a></li>);
-  };
-    
+  
   return (
     <Container fluid>
       <Row className='vh-100'>
         <Col sm={2}>
-          <h3>Channels</h3>
-          <ul>
-            {renderChannels(channels)}
-          </ul>
+          <h5>Channels</h5>
+            <Channels channels={channels} />
         </Col>
         <Col sm={10}>
           <ChatRoom />
---
+++ src/components/Channels.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Channels = ({ channels }) => {
+  const renderChannels = channels.map((channel) => <li key={channel.name} className='list-group-item'><a href='#'>{channel.name}</a></li>);
+
+  return (
+    <ul className='list-group list-group-flush'>
+      { renderChannels }
+    </ul>
+  ); 
+};
+
+export default Channels;
