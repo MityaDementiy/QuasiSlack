afac8f99740c2bd73b83c84c6c9e8075d0874d60
MityaDementiy <d.dementiy@gmail.com>
2020-10-18 17:37:34 +0300

    Configure store

--- src/initial.jsx
+++ src/initial.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import { setUserName, getUserName } from './utils';
+import rootReducer from './reducers';
 
 if (!getUserName()) {
   setUserName();
@@ -12,12 +15,18 @@ console.log(userName);
 
 const UserContext = React.createContext();
 
+const store = configureStore({
+  reducer: rootReducer,
+});
+
 export default (channels) => {
   const container = document.querySelector('.container');
   ReactDom.render(
-    <UserContext.Provider value={userName}>
-      <App channels={channels} />
-    </UserContext.Provider>,
+    <Provider store={store}>
+      <UserContext.Provider value={userName}>
+        <App channels={channels} />
+      </UserContext.Provider>
+    </Provider>,
     container,
   );
 };
