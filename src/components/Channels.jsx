836fe2f1e5eb87ff29909fa3aa5aeb11de50a807
MityaDementiy <d.dementiy@gmail.com>
2020-10-28 20:54:04 +0200

    Add select channel functionality

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import cn from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectChannel } from '../features/channels/channelsSlice';
 
-const Channels = ({ channels, currentChannelId }) => {
+const Channels = () => {
+  const dispatch = useDispatch();
+  const channels = useSelector((state) => state.channels.channels);
+  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+
   const getClasses = (id) => {
     const buttonType = currentChannelId === id ? 'primary' : 'secondary';
     const classNames = cn('btn', 'mb-3', `btn-${buttonType}`);
     return classNames;
   };
 
+  const handleSelectChannel = (e) => {
+    e.preventDefault();
+    const selectedChannelId = parseInt(e.target.id, 10);
+    dispatch(selectChannel(selectedChannelId));
+  };
+
   const renderChannels = channels
-    .map((c) => <button key={c.name} className={getClasses(c.id)}>{c.name}</button>);
+    .map((c) => <button key={c.name} id={c.id} className={getClasses(c.id)} onClick={handleSelectChannel}>{c.name}</button>);
 
   return (
     <div className='btn-group-vertical'>
--- src/features/channels/channelsSlice.js
+++ src/features/channels/channelsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import update from 'immutability-helper';
 
 const channelsSlice = createSlice({
   name: 'channels',
@@ -6,6 +7,15 @@ const channelsSlice = createSlice({
     channels: [{ id: 1, name: 'general', removable: false }, { id: 2, name: 'random', removable: false }],
     currentChannelId: 1,
   },
+  reducers: {
+    selectChannel(state, action) {
+      const id = action.payload;
+      const key = 'currentChannelId';
+      return update(state, { [key]: { $set: id } });
+    },
+  },
 });
+
+export const { selectChannel } = channelsSlice.actions;
 
 export default channelsSlice.reducer;
