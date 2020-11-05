bd68a0b7e46b85e529042a3670d0a674ef77d1bf
MityaDementiy <d.dementiy@gmail.com>
2020-11-05 19:59:02 +0200

    Install rollbar

--- src/initial.jsx
+++ src/initial.jsx
@@ -3,11 +3,17 @@ import ReactDom from 'react-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import io from 'socket.io-client';
+import Rollbar from "rollbar";
 import App from './components/App';
 import { setUserName, getUserName, UserContext } from './utils';
 import rootReducer from './reducers';
 import { addMessage, initMessages } from './features/messages/messagesSlice';
-import { addChannel, initChannels, removeChannel, renameChannel } from './features/channels/channelsSlice';
+import {
+  addChannel,
+  initChannels,
+  removeChannel,
+  renameChannel,
+} from './features/channels/channelsSlice';
 
 export default ({ channels, currentChannelId, messages }) => {
   if (!getUserName()) {
@@ -35,6 +41,12 @@ export default ({ channels, currentChannelId, messages }) => {
   });
   socket.on('renameChannel', ({ data: { attributes } }) => {
     store.dispatch(renameChannel(attributes));
+  });
+
+  const rollbar = new Rollbar({
+    accessToken: 'a36c303f356b4c8aa59ef1713b1fc2c1',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
   });
 
   const container = document.querySelector('.container');
