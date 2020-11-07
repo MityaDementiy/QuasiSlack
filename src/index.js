e2830efdc407fbc2fe28ecdb028bc07b79215054
MityaDementiy <d.dementiy@gmail.com>
2020-11-07 18:48:54 +0200

    Delete commented codelines and console logs

--- src/index.js
+++ src/index.js
@@ -5,18 +5,11 @@ import 'regenerator-runtime/runtime';
 
 import '../assets/application.scss';
 
-// import faker from 'faker';
 import gon from 'gon';
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
 import run from './initial';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-console.log('it works!');
-console.log('gon', gon);
-
-// const { channels, currentChannelId } = gon;
 run(gon);
