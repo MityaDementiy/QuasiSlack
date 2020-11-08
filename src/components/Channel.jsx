f06b27ea273239a81aa56bbce32ea960cb165b50
MityaDementiy <d.dementiy@gmail.com>
2020-11-08 13:46:58 +0200

    Refactor channels component

---
+++ src/components/Channel.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import cn from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+import { Edit, Trash } from 'react-feather';
+
+import { selectChannel } from '../slices/channelsSlice';
+import { isRemovable } from '../utils';
+import { openModal } from '../slices/modalsSlice';
+
+const Channel = () => {
+  const dispatch = useDispatch();
+  const channels = useSelector((state) => state.channels.channels);
+  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+
+  const getClasses = (id) => {
+    const buttonType = currentChannelId === id ? 'primary' : 'secondary';
+    const classNames = cn('btn', 'mb-3', `btn-${buttonType}`, 'btn-lg');
+    return classNames;
+  };
+
+  const handleSelectChannel = (e) => {
+    e.preventDefault();
+    const targetElement = e.target;
+    if (targetElement.innerHTML === 'general' || targetElement.innerHTML === 'random') {
+      dispatch(selectChannel(parseInt(targetElement.id, 10)));
+    } else {
+      const closestButtonGroup = targetElement.closest('.btn-group');
+      dispatch(selectChannel(parseInt(closestButtonGroup.id, 10)));
+    }
+  };
+
+  const handleShowDeleteModal = (e) => {
+    e.preventDefault();
+    dispatch(openModal('removing'));
+  };
+
+  const handleShowRenameModal = (e) => {
+    e.preventDefault();
+    dispatch(openModal('renaming'));
+  };
+
+  return channels
+    .map((c) => {
+      const classes = getClasses(c.id);
+      if (!isRemovable(c)) {
+        return (
+          <button
+          key={`${c.name}, ${c.id}`}
+          id={c.id}
+          className={classes}
+          onClick={handleSelectChannel}
+          >
+          {c.name}
+          </button>
+        );
+      }
+      return (
+        <div className='btn-group' key={`${c.name}, ${c.id}`} onClick={handleSelectChannel} id={c.id}>
+          <button className={`${classes}, w-50`}>
+            {c.name}
+          </button>
+          <button className={classes} onClick={handleShowDeleteModal}>
+            <Trash />
+          </button>
+          <button className={classes} onClick={handleShowRenameModal}>
+            <Edit />
+          </button>
+        </div>
+      );
+    });
+};
+
+export default Channel;
--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,79 +1,10 @@
 import React from 'react';
-import cn from 'classnames';
-import { useDispatch, useSelector } from 'react-redux';
-import { Edit, Trash } from 'react-feather';
+import Channel from './Channel';
 
-import { selectChannel } from '../slices/channelsSlice';
-import { isRemovable } from '../utils';
-import { openModal } from '../slices/modalsSlice';
-
-const Channels = () => {
-  const dispatch = useDispatch();
-  const channels = useSelector((state) => state.channels.channels);
-  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
-
-  const getClasses = (id) => {
-    const buttonType = currentChannelId === id ? 'primary' : 'secondary';
-    const classNames = cn('btn', 'mb-3', `btn-${buttonType}`, 'btn-lg');
-    return classNames;
-  };
-
-  const handleSelectChannel = (e) => {
-    e.preventDefault();
-    const targetElement = e.target;
-    if (targetElement.innerHTML === 'general' || targetElement.innerHTML === 'random') {
-      dispatch(selectChannel(parseInt(targetElement.id, 10)));
-    } else {
-      const closestButtonGroup = targetElement.closest('.btn-group');
-      dispatch(selectChannel(parseInt(closestButtonGroup.id, 10)));
-    }
-  };
-
-  const handleShowDeleteModal = (e) => {
-    e.preventDefault();
-    dispatch(openModal('removing'));
-  };
-
-  const handleShowRenameModal = (e) => {
-    e.preventDefault();
-    dispatch(openModal('renaming'));
-  };
-
-  const renderChannels = channels
-    .map((c) => {
-      const classes = getClasses(c.id);
-      if (!isRemovable(c)) {
-        return (
-          <button
-            key={`${c.name}, ${c.id}`}
-            id={c.id}
-            className={classes}
-            onClick={handleSelectChannel}
-          >
-            {c.name}
-          </button>
-        );
-      }
-      return (
-        <div className='btn-group' key={`${c.name}, ${c.id}`} onClick={handleSelectChannel} id={c.id}>
-          <button className={`${classes}, w-50`}>
-            {c.name}
-          </button>
-          <button className={classes} onClick={handleShowDeleteModal}>
-            <Trash />
-          </button>
-          <button className={classes} onClick={handleShowRenameModal}>
-            <Edit />
-          </button>
-        </div>
-      );
-    });
-
-  return (
-    <div className='btn-group-vertical d-block'>
-      { renderChannels }
-    </div>
-  );
-};
+const Channels = () => (
+  <div className='btn-group-vertical d-block'>
+    <Channel />
+  </div>
+);
 
 export default Channels;
