22ac1509016b8b702c825b2ebc583dcf5e6f9455
MityaDementiy <d.dementiy@gmail.com>
2020-12-05 19:52:29 +0200

    Refactor validator

--- src/validator.js
+++ src/validator.js
@@ -6,15 +6,15 @@ export const validateChannels = (channels) => {
   return Yup.object().shape({
     name: Yup.string()
       .trim()
-      .min(3, 'Too short, minimum length is 3 symbols!')
-      .max(10, 'Too long, maximum length is 10 symbols!')
-      .notOneOf(channelsNames, 'Channel name must be unique!')
-      .required('Required'),
+      .min(3)
+      .max(10)
+      .notOneOf(channelsNames)
+      .required(),
   });
 };
 
 export const submitMessageSchema = Yup.object().shape({
   message: Yup.string()
     .trim()
-    .min(1, 'You can not submit empty form'),
+    .min(1),
 });
